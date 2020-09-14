9bde2a831eed86dfe191d89f15bb77d730473fa9
Carlos Daniel Contreras <tiburonchan3@gmail.com>
2020-09-14 12:40:17 -0600

    LocalStorage y useEffect + JWT token decode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import SingInSingUp from './pages/SingInSingUp';
 import {ToastContainer} from 'react-toastify'
+import {authContext} from './utils/context';
+import {isUserLoggedApi} from './api/auth'
+// import {API_HOST, TOKEN} from './utils/constants';
+// import JwtDecode from 'jwt-decode'
 
 
 export default function App() {
     const [user,setUser] = useState(null);
+    useEffect(() => {
+      setUser(isUserLoggedApi())
+    }, [])
     return(
-        <div>
+        <authContext.Provider value={user}>
             {user ?(
                 <div>
-                    <h1>no estas loggueado</h1>
+                    <h1>Estas loggueado</h1>
                 </div>
             ) : (
                 <div>
@@ -26,7 +33,7 @@ export default function App() {
                 draggable
                 pauseOnHover
             />
-        </div>
+        </authContext.Provider>
     );
 }
 
